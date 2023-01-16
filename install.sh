27acc07b8f42e163155a6c89b05d40ae49d0650a
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2023-01-16 08:50:03 -0300

    fix: fixing the path of symbolic link used to miomi

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 chmod +x src/main.py
 pip3 install -r requirements.txt;
 sudo mv ../miomi /usr/local/bin/
-sudo ln -sf /usr/local/bin/miomi/main.py /usr/bin/miomi
+sudo ln -sf /usr/local/bin/miomi/src/main.py /usr/bin/miomi
