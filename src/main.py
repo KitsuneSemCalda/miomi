85df3cd7801a3b5c1f77ec5a4a98c93054fadbf9
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2023-01-16 06:59:42 -0300

    feat: update main.py to englobe the queue_controller

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import argparse
+import queue_controller
+
 
 def setup_args():
     parse = argparse.ArgumentParser(
@@ -7,10 +9,23 @@ def setup_args():
         epilog="Developed from KitsuneSemCalda",
         usage="%(prog)s [OPTIONS]:",
         )
+    subparse = parse.add_subparsers(title="subcommands", description="subcommands for miomi", dest="subcommand")
+    
+    add_queue = subparse.add_parser("addqueue", description="subcommand to add a new url to queue")
+    add_queue.add_argument("url")
+
+    subparse.add_parser("clearqueue", description="subcommand to clean a queue file")
+    subparse.add_parser("downloadqueue", description="subcommand to download links in queue")
     return parse.parse_args()
 
 def main():
-    setup_args()
+    args = setup_args()
+    if args.subcommand == "addqueue":
+        queue_controller.incrementNewLink(queue_controller.filepath, args.url)
+    if args.subcommand == "clearqueue":
+        queue_controller.clearQueue(queue_controller.filepath)
+    if args.subcommand == "downloadqueue":
+        queue_controller.downloadQueue(queue_controller.filepath)
 
 if __name__ == "__main__":
     main()
