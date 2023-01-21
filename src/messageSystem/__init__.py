2d46dc67a5470ae29ae0cd5502a7a7e31f274953
KitsuneSemCalda <59454155+KitsuneSemCalda@users.noreply.github.com>
2023-01-21 13:39:59 -0300

    Delete src/messageSystem directory

--- src/messageSystem/__init__.py
+++
@@ -1,8 +0,0 @@
-def alert_message(message):
-    print(f"\033[93m {message} \033[00m")
-
-def warning_message(message):
-    print(f"\033[91m {message} \033[00m")
-
-def sucess_message(message):
-    print(f"\033[92m {message}\033[00m")
